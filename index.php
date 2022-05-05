2f8420c254903206ecb4b240f57baf5877251faa
André Malveira <andremalveira.dev@gmail.com>
2022-05-04 23:19:17 -0300

    updated

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
   $title        = $_GET['t'] ?? $_GET['title']        ?? null;
   $description  = $_GET['d'] ?? $_GET['description']  ?? null;
   $image        = $_GET['i'] ?? $_GET['image']        ?? null;
-
+  $site_name    = $_GET['sn'] ?? $_GET['site_name']   ?? null;
 
 ?>
 
@@ -15,28 +15,34 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
   <!-- meta to shared --> 
-  <meta property="og:site_name"        content="astlibjs.ga"> 
+  <meta property="og:site_name"         content="<?php echo $site_name     ?? 'sharin.gq';?>"> 
   <!-- FACEBOOK -->
-  <meta property="og:url" content="<?php echo $url;?>">
-  <meta property="og:title" content="<?php echo $title;?>">
-  <meta property="og:description" content="<?php echo $description;?>">
-  <meta property="og:image" content="<?php echo $image;?>">
+  <meta property="og:url"               content="<?php echo $url           ?? 'https://sharin.gq';?>">
+  <meta property="og:title"             content="<?php echo $title         ?? 'Sharing';?>">
+  <meta property="og:description"       content="<?php echo $description   ?? 'Custom preview link to share';?>">
+  <meta property="og:image"             content="<?php echo $image         ?? '';?>">
 
 
   <!-- TWITTER -->
-  <meta name="twitter:url" content="<?php echo $url;?>">
-  <meta name="twitter:title" content="<?php echo $title;?>">
-  <meta name="twitter:description" content="<?php echo $description;?>">
-  <meta name="twitter:image" content="<?php echo $image;?>">
+  <meta name="twitter:url"              content="<?php echo $url           ?? 'https://sharin.gq' ;?>">
+  <meta name="twitter:title"            content="<?php echo $title         ?? 'Sharing' ;?>">
+  <meta name="twitter:description"      content="<?php echo $description   ?? 'Custom preview link to share' ;?>">
+  <meta name="twitter:image"            content="<?php echo $image         ?? '' ;?>">
 
 
-  <title>Redirecting...</title>
-  <?php if($url) {
-    echo '<script>window.location.replace('.$url.')</script>';
-  }?>
+  <title><?php echo $title;?></title>
+  <script>window.location.replace('<?php echo $url ?? '/getting-starter';?>')</script>
+
+  <link rel="stylesheet" href="getting-starter/src/css/loading.css">
 
 </head>
-  <style>body {background: #222;color: #eee;}</style>
-  <body></body>
+  <style>body {background: #151515;}</style>
+  <body>
+    <div id="loading">
+      <svg class="spinner" viewBox="0 0 50 50">
+        <circle class="path" cx="25" cy="25" r="20" style="stroke:#539bf5;" fill="none" stroke-width="3"></circle>
+      </svg>
+    </div>
+  </body>
 </html>
 
