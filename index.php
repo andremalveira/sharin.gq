b76c95df0065afbe065507f26e86719fe3ba172b
André Malveira <andremalveira.dev@gmail.com>
2022-05-07 16:26:24 -0300

    updated index.html

--- index.php
+++ index.php
@@ -1,9 +1,37 @@
 <?php
+  function urlValidate($url){
+    $url_valid = $url;
+    $urlIsHttp = false;
+    $noHasProtocol = false;
+
+    $scheme = parse_url($url)['scheme'] ?? null;
+    $host = parse_url($url)['host'] ?? null;
+
+
+    if($scheme){
+      if($scheme == 'http'){$urlIsHttp = true;};
+
+    } else {
+      $host = parse_url('https://'.$url)['host'];
+      $noHasProtocol = true;
+    }
+
+     return  [
+      "url" => $url_valid,
+      "scheme" => $scheme,
+      "host" => $host,
+      "urlIsHttp" => $urlIsHttp,
+      "noHasProtocol" => $noHasProtocol
+    ];
+  }
+
   $url          = $_GET['u'] ?? $_GET['url']          ?? null;
+  $url          = urlValidate($url)['url'];
+
   $title        = $_GET['t'] ?? $_GET['title']        ?? null;
   $description  = $_GET['d'] ?? $_GET['description']  ?? null;
   $image        = $_GET['i'] ?? $_GET['image']        ?? null;
-  $site_name    = $_GET['sn'] ?? $_GET['site_name']   ?? $url     ?? null;
+  $site_name    = $_GET['sn'] ?? $_GET['site_name']   ?? urlValidate($url)['host']   ?? null;
 
 ?>
 <!-- Author: https://github.com/andremalveira -->
@@ -32,18 +60,40 @@
   <meta name="twitter:card"             content="summary_large_image">
 
   <title><?php echo $title ?? 'Sharin' ;?></title>
-  <script>window.location.replace('<?php echo $url ?? '/getting-starter';?>')</script>
-
+  
+  <link rel="stylesheet" href="index.style.css">
   <link rel="stylesheet" href="getting-starter/src/css/loading.css">
 
 </head>
-  <style>body {background: #151515;}</style>
   <body>
-    <div id="loading">
-      <svg class="spinner" viewBox="0 0 50 50">
-        <circle class="path" cx="25" cy="25" r="20" style="stroke:#539bf5;" fill="none" stroke-width="3"></circle>
-      </svg>
-    </div>
+  <?php if(!urlValidate($url)['urlIsHttp'] && !urlValidate($url)['noHasProtocol']){ 
+      echo'<script>window.location.replace(\''.($url ?? '/getting-starter').' \')</script>';
+      echo 
+      '<div id="loading">
+          <svg class="spinner" viewBox="0 0 50 50">
+            <circle class="path" cx="25" cy="25" r="20" style="stroke:#539bf5;" fill="none" stroke-width="3"></circle>
+          </svg>
+        </div>
+      ';
+    }
+  ?>
+
+    <?php if(urlValidate($url)['urlIsHttp'] or urlValidate($url)['noHasProtocol']){  
+      echo '
+        <div class="warning">
+          <i class="icon">⚠️</i>
+          <div class="message">
+            The url you are being redirected to does not have an HTTPS protocol, meaning it could be an untrusted website!
+          </div>
+          <div class="cod">Not Safe:<span> '.$url.'</span></div>
+          <button onclick="window.location.replace(\''.$url.'\')">
+            Do you want to proceed anyway?
+          </button>
+        </div>
+        ';
+      };
+    ?>
+
   </body>
 </html>
 
